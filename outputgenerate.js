5dc7814555666ec766e9dcf3baca0d3d2b2c1f6e
harshpeswani45 <harshpeswani45@gmail.com>
2018-11-05 15:46:46 +0530

    New Output Generate

--- outputgenerate.js
+++ outputgenerate.js
@@ -1,6 +1,11 @@
  
   var fs=require('fs')
   var s=require('./inputread.js')
+  var isBook=require('./isBook.js')
+  var isMusic=require('./isMusic.js')
+  var isChoclate=require('./isChoclate.js')
+  var isOther=require('./isOther.js')
+
   var output=""
   var o=s.indexOf('Input')
   var i=0,j=0 
@@ -25,98 +30,40 @@
       total=0
       output+="\n\nOutput :"+s[o+6]+"\n\n"
       o=s.indexOf('Input',o+1)
-      //console.log(o)
-      //console.log(output)
       count++
     }
     
 
     var sub=s.substring(i,j)
-    if(sub.indexOf('book')!=-1)
-    {
-      output+=sub.substring(0,sub.indexOf('at')-1)
-      total+=sub.substring(sub.indexOf('t')+2,j)*1
-      output+=" : "+sub.substring(sub.indexOf('t')+2,j)+"\n"
-      //console.log(output)
-        
-    } 
-    if(sub.indexOf('music')!=-1)  
-    {
-      output+=sub.substring(0,sub.indexOf('at')-1)
-      var price
-     
-      salestax+=0.1*sub.substring(sub.indexOf('t')+2,j)
-      var price=sub.substring(sub.indexOf('t')+2,j)*1+Math.round(0.1*sub.substring(sub.indexOf('t')+2,j)*10)/10
-      total+=price
-      output+=" : "+price+"\n"
-      //console.log(output)
-    } 
-
-    if(sub.indexOf('chocolate')!=-1)  
-    {
-      if(sub.indexOf('imported')==-1)
-      {
-        output+=sub.substring(0,sub.indexOf('at',10)-1)
-        var price
-        var price=sub.substring(sub.indexOf('t',10)+2,j)*1
-        total+=price
-        output+=" : "+price+"\n"
-        //console.log(output)
-                     
-      }
-      else
-      {
-        output+=sub.substring(0,sub.indexOf('at',26)-1)
-        var price
-        var price=sub.substring(sub.indexOf('t',26)+2,j)*1+Math.round(0.05*sub.substring(sub.indexOf('t',26)+2,j)*10)/10
-        total+=price
-        output+=" : "+price+"\n"
-        //console.log(output)
-      }
-    }
-    if(sub.indexOf('perfume')!=-1)
-    {
-        if(sub.indexOf('imported')==-1)
-        {
-          output+=sub.substring(0,sub.indexOf('at')-1)
-          var price
-          salestax+=0.1*sub.substring(sub.indexOf('at')+3,j)
-          var price=sub.substring(sub.indexOf('at')+3,j)*1+Math.round(0.1*sub.substring(sub.indexOf('at')+3,j)*10)/10
-          total+=price
-          output+=" : "+price+"\n"
-
-        }
-        else
-        {
-            output+=sub.substring(0,sub.indexOf('at')-1)
-            var price
-            salestax+=0.1*sub.substring(sub.indexOf('at')+3,j)
-            //console.log(sub.substring(sub.indexOf('at')+3,j))
-            var price=sub.substring(sub.indexOf('at')+3,j)*1+Math.round(0.1*sub.substring(sub.indexOf('at')+3,j)*10)/10+0.05*sub.substring(sub.indexOf('at')+3,j)
-            total+=price
-            output+=" : "+price+"\n"
-            
-        }
-    }
-    if(sub.indexOf('pills')!=-1)  
-    {
-      if(sub.indexOf('imported')==-1)
-      {
-        output+=sub.substring(0,sub.indexOf('at')-1)
-        var price
-        var price=sub.substring(sub.indexOf('at')+3,j)*1
-        total+=price
-        output+=" : "+price+"\n"
-        //console.log(output)
-        //break;
-      }
-      
-    }
+    isBook(sub,output,total,i,j,function(result,newi,newj,newtotal){
+      output=result
+      i=newi
+      j=newj
+      total=newtotal
+    })
+    isMusic(sub,output,total,salestax,i,j,function(result,newi,newj,newtotal,newsalestax){
+      output=result
+      i=newi
+      j=newj
+      total=newtotal
+      salestax=newsalestax
+    })
+    isChoclate(sub,output,total,salestax,i,j,function(result,newi,newj,newtotal,newsalestax){
+      output=result
+      i=newi
+      j=newj
+      total=newtotal
+      salestax=newsalestax
+    })
+    isOther(sub,output,total,salestax,i,j,function(result,newi,newj,newtotal,newsalestax){
+      output=result
+      i=newi
+      j=newj
+      total=newtotal
+      salestax=newsalestax
+    })
     i=j+1;
     j=s.indexOf('\n',j+1)
-    
-     
-
   }while(i<last)
   output+="\n\n\nSales Tax="+salestax
   output+="\nTotal :"+total
